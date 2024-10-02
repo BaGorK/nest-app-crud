ac4b673736663651f5d2b64cb2c38dec91ed2179
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-10-02 17:42:49 +0300

    feat: create read update delete users implemented

---
+++ src/users/users.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Get()
+  getAllUsers() {
+    return this.userService.getAllUsers();
+  }
+
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto);
+  }
+
+  @Get(':id')
+  getUser(@Param('id') id: string) {
+    return this.userService.getUser(id);
+  }
+
+  @Patch(':id')
+  updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.updateUser(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    return this.userService.deleteUser(id);
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  users: CreateUserDto[] = [];
+
+  getAllUsers() {
+    return {
+      status: 'success',
+      numOfResults: this.users.length,
+      users: this.users,
+    };
+  }
+
+  createUser(createUserDto: CreateUserDto) {
+    const user = createUserDto;
+    user.id = this.users.length + 1;
+    this.users.push(user);
+
+    return { status: 'success', data: { user } };
+  }
+
+  getUser(id: string) {
+    const user = this.users.find((user) => user.id === +id);
+    console.log(user);
+
+    return { status: 'success', data: { user } };
+  }
+
+  updateUser(id: string, updateUserDto: UpdateUserDto) {
+    const index = this.users.findIndex((user) => user.id === +id);
+    const user = {
+      ...this.users[index],
+      ...updateUserDto,
+    };
+    this.users[index] = user;
+
+    return { status: 'success', data: { user } };
+  }
+
+  deleteUser(id: string) {
+    this.users = this.users.filter((user) => user.id !== +id);
+
+    return { status: 'success', data: null };
+  }
+}
