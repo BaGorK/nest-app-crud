a855cf9a33a3f97303e48c34f5420edde69b738f
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-10-10 07:51:42 +0300

    feat: update and refactor user services

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { User } from './user.entity';
+import { userData } from 'src/data/user';
 
 @Injectable()
 export class UsersService {
-  users: CreateUserDto[] = [
-    { id: 1, name: 'john', age: 23 },
-    { id: 2, name: 'Doe', age: 33 },
-    { id: 3, name: 'bob', age: 12 },
-  ];
+  users: User[] = userData;
 
   getAllUsers() {
     return {
@@ -19,33 +17,37 @@ export class UsersService {
   }
 
   createUser(createUserDto: CreateUserDto) {
-    const user = createUserDto;
-    user.id = this.users.length + 1;
+    const user: User = {
+      ...createUserDto,
+      id: this.users.length + 1,
+      dateOfBirth: new Date(createUserDto.dateOfBirth),
+    };
     this.users.push(user);
 
     return { status: 'success', data: { user } };
   }
 
-  getUser(id: string) {
-    const user = this.users.find((user) => user.id === +id);
+  getUser(id: number) {
+    const user = this.users.find((user) => user.id === id);
     console.log(user);
 
     return { status: 'success', data: { user } };
   }
 
-  updateUser(id: string, updateUserDto: UpdateUserDto) {
-    const index = this.users.findIndex((user) => user.id === +id);
-    const user = {
+  updateUser(id: number, updateUserDto: UpdateUserDto) {
+    const index = this.users.findIndex((user) => user.id === id);
+    const user: User = {
       ...this.users[index],
       ...updateUserDto,
+      dateOfBirth: new Date(updateUserDto.dateOfBirth),
     };
     this.users[index] = user;
 
     return { status: 'success', data: { user } };
   }
 
-  deleteUser(id: string) {
-    this.users = this.users.filter((user) => user.id !== +id);
+  deleteUser(id: number) {
+    this.users = this.users.filter((user) => user.id !== id);
 
     return { status: 'success', data: null };
   }
